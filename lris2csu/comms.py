4541b6b5cc8958e3413c73cb567cf485bfe3ada7
Jeb Bailey <jibailey@caltech.edu>
2025-03-03 16:35:39 -0800

    zero out comms.py for now.

--- lris2csu/comms.py
+++ lris2csu/comms.py
@@ -1,12 +1,1 @@
-#Stub placeholder for pyzmq, mKTL, redis pubsub, python socket, etc ad nauseum communication wrapper
-
-
-class Communicator:
-    def send(self, who, what):
-        pass
-
-    def listen(self, who: str | List[str], filter=None):
-        yield None
-
-    def receive(self, who: str | List[str], filter=None):
-        pass
+#Stub placeholder for pyzmq, mKTL, redis pubsub, python socket, etc ad nauseam communication wrapper
