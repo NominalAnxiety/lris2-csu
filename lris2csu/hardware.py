874e0c4bd7f6f47a67268299a56e39321d734110
Jeb Bailey <jibailey@caltech.edu>
2025-03-14 18:41:23 -0700

    Optimize EtherCAT bus locking and implement bits needed for slit relative positioning. Actually looks like a set of slits.
    Introduced thread-safe locking for EtherCAT bus operations using RLock to prevent concurrency issues. Updated configuration in `csu.yaml` (`um_per_count` added) and include positional offsets. Refactored hardware logic to compute bar positions and added motor stop functionality in `epos4bus.py`.

--- lris2csu/hardware.py
+++ lris2csu/hardware.py
@@ -2,50 +2,18 @@ from logging import getLogger
 from cooethercat import EPOS4Motor
 from cooethercat.helpers import make_pdo_mapping
 import yaml
-from typing import NamedTuple
-
-
-# class CSU_motor_config_defaults(NamedTuple):
-#     motor_type: int = 10
-#     nominal_current_ma: int = 1000
-#     output_current_limit_ma: int = 1000
-#     number_of_pole_pairs: int = 7
-#     thermal_time_constant_winding_ms: int = 100
-#     torque_constant_uNm_A: int = 100
-#     max_motor_speed_rpm: int = 1000
-#
-# class CSU_gear_config_defaults(NamedTuple):
-#     gear_reduction_numerator: int = 1
-#     gear_reduction_denominator: int = 1
-#     gear_max_input_speed_rpm: int = 1000
-#     orientation: int = 1
-#
-# class CSU_digital_incremental_encoder_config_defaults(NamedTuple):
-#     number_of_pulses_per_turn: int = 1000
-#     encoder_type: int = 1
-#     direction: int = 1
-#     method: int = 1
-#
-# class CSU_ssi_encoder_config_defaults(NamedTuple):
-#     data_rate: int = 1000000
-#     number_of_bits: int = 24
-#     encoding_type: int = 1
-#     direction: int = 1
-#     check_frame: int = 1
-#     timeout_time_ms: int = 1000
-#     number_of_multi_turn_bits: int = 12
-#     number_of_single_turn_bits: int = 12
-
 
 
 class CSUHardwareConfig:
     yaml_tag = u'!CSUHardwareConfig'
+
 
     def __init__(self, ethercat_device:str, bar_pairs:list["BarPairConfig"], left_brake, right_brake):
         self.ethercat_device = ethercat_device
         self.bar_pairs = {x.id: x for x in bar_pairs}
         self.left_brake = left_brake
         self.right_brake = right_brake
+        self.opening_width_mm = 260
 
     @classmethod
     def from_yaml(cls, loader, node):
@@ -64,6 +32,44 @@ class CSUHardwareConfig:
         }
         return dumper.represent_mapping(cls.yaml_tag, mapping)
 
+    def compute_bar_count_positions(self, pos_width_dict: dict[int, tuple[float, float]]) -> dict:
+        """
+        Computes the bar count positions for a given bar pair configuration.
+
+        Args:
+            pos_width_dict: A dictionary keyed on bar pair id with a tuple (position, width) in mm.
+            bar_pair: An instance of BarPairConfig containing 'id', 'left' (BarConfig), and 'right' (BarConfig).
+
+        Returns:
+            A dictionary with bus_id as keys and the computed bar count positions as values.
+            For the left side, count position = (position - width/2) / left.um_per_count.
+            For the right side, count position = (position + width/2) / right.um_per_count.
+        """
+        # Retrieve the requested (position, width) tuple for this bar pair.
+        ret = {}
+        for id in pos_width_dict:
+
+            if id not in self.bar_pairs:
+                raise KeyError(f"No bar pair with id {id} found in the bar pair configuration.")
+
+            position, width = pos_width_dict[id]
+            bar_pair = self.bar_pairs[id]
+
+            # Compute the absolute physical positions in mm for left and right.
+            # lower left is origin
+            left_mm = (position - width / 2.0) - bar_pair.left.offset_mm
+            right_mm = (position + width / 2.0)
+
+            right_travel_mm = self.opening_width_mm - right_mm - bar_pair.right.offset_mm
+
+            # Convert mm to counts using the respective um_per_count conversion factors.
+            left_count = left_mm*1000 / bar_pair.left.um_per_count
+            right_count = right_travel_mm*1000 / bar_pair.right.um_per_count
+
+            ret[bar_pair.left.bus_id] = int(round(left_count))
+            ret[bar_pair.right.bus_id] = int(round(right_count))
+
+        return ret
 
 # Register the representer and constructor with PyYAML
 yaml.add_representer(CSUHardwareConfig, CSUHardwareConfig.to_yaml)
@@ -99,23 +105,24 @@ yaml.add_constructor(u'!BarPairConfig', BarPairConfig.from_yaml)
 class BarConfig:
     yaml_tag = u'!BarConfig'
 
-    def __init__(self, bus_id, speed=0, torque_limit=0, mm_per_count=1.0, reversed=False):
+    def __init__(self, bus_id, speed:int=0, torque_limit=0, um_per_count:float=1.0, reversed:bool=False, offset_mm:float=0):
         self.bus_id = bus_id
         self.speed = speed
         self.torque_limit = torque_limit
-        self.mm_per_count = mm_per_count
+        self.um_per_count = um_per_count
         self.reversed = reversed
-        assert self.mm_per_count!=0
-        if self.mm_per_count<0:
-            assert reversed==True, 'If mm_per_count is negative, reversed must be True'
-        if reversed and self.mm_per_count>0:
-            getLogger(__name__).warning(f'Inverting mm_per_count ({mm_per_count}) for bus_id {bus_id} as reversed is set.')
-            self.mm_per_count=-self.mm_per_count
+        self.offset_mm = offset_mm
+        assert self.um_per_count!=0
+        if self.um_per_count<0:
+            assert reversed==True, 'If um_per_count is negative, reversed must be True'
+        if reversed and self.um_per_count>0:
+            getLogger(__name__).warning(f'Inverting um_per_count ({um_per_count}) for bus_id {bus_id} as reversed is set.')
+            self.um_per_count=-self.um_per_count
 
     def __repr__(self):
         return (f"BarMotor(bus_id={self.bus_id}, speed={self.speed}, "
-                f"torque_limit={self.torque_limit}, mm_per_count={self.mm_per_count}, "
-                f"reversed={self.reversed})")
+                f"torque_limit={self.torque_limit}, um_per_count={self.um_per_count}, "
+                f"reversed={self.reversed} offset_mm={self.offset_mm})")
 
     @classmethod
     def from_yaml(cls, loader, node):
@@ -131,8 +138,9 @@ class BarConfig:
             'bus_id': data.bus_id,
             'speed': data.speed,
             'torque_limit': data.torque_limit,
-            'mm_per_count': data.mm_per_count,
-            'reversed': data.reversed
+            'um_per_count': data.um_per_count,
+            'reversed': data.reversed,
+            'offset_mm': data.offset_mm,
         }
         return dumper.represent_mapping(cls.yaml_tag, mapping)
 
@@ -156,7 +164,7 @@ class BarMotor(EPOS4Motor):
     #     return {}
 
     def config_func(self, bus_id):
-        getLogger(__name__).debug(f"Configuring device {self} via config_func (EPOS4 Micro 24/5)")
+        getLogger(__name__).debug(f"Configuring BarMotor device {self} via config_func (EPOS4 Micro 24/5) at bus node {self.node}")
         assert bus_id == self.node
 
         # Define the Process Data Objects for PPM (Rx and Tx)
@@ -205,7 +213,8 @@ class BarMotor(EPOS4Motor):
         # Set the home offset move distance
         getLogger(__name__).debug(f"Configuring device {self} complete.")
 
-
+    #TODO this is a bit of copypasta that will be useful for fully configuring an EPOS4 from sratch based on Jake's
+    # early lab scripts
     # def config_func(self, motor_configuration=CSU_motor_config_defaults(),
     #                     gear_configuration=CSU_gear_config_defaults(),
     #                     digital_incremental_encoder_configuration=CSU_digital_incremental_encoder_config_defaults(),
@@ -217,11 +226,9 @@ class BarMotor(EPOS4Motor):
     #     @param digital_incremental_encoder_configuration: the digital incremental encoder configuration
     #     @param ssi_encoder_configuration: the SSI encoder configuration
     #     """
-    #
     #     if slave == None:
     #         logging.error('configure_slave: no slave available')
     #         return
-    #
     #     set_node_id(slave, 1)
     #     set_motor_data(slave, motor_configuration.nominal_current_ma, motor_configuration.output_current_limit_ma,
     #                    motor_configuration.thermal_time_constant_winding_ms, motor_configuration.torque_constant_uNm_A)
@@ -240,17 +247,17 @@ class BarMotor(EPOS4Motor):
     #     slave.dc_sync(act=True, sync0_cycle_time=1000000)
 
 
-class Break(EPOS4Motor):
+class BreakMotor(EPOS4Motor):
     def engage(self):
-        pass
+        raise NotImplementedError
 
     def disengage(self):
-        pass
+        raise NotImplementedError
 
     def config_func(self, bus_id):
-        """ Configures an EPOS4 Micro TRB 12CC device """
-        getLogger(__name__).debug(f"Configuring device {self} (EPOS4 Micro 24/5)")
-        assert bus_id == self.id  #TODO check that this is actually an attribute
+        getLogger(__name__).debug(f"Configuring BreakMotor device {self} via "
+                                  f"config_func (EPOS4 Micro 24/5) at bus node {self.node}")
+        assert bus_id == self.node
             #
         # Define the Process Data Objects for PPM (Rx and Tx)
         ppm_rx = [
@@ -295,6 +302,38 @@ class Break(EPOS4Motor):
         self.sdo_write(self.object_dict.DIGITAL_INPUT_CONFIGURATION_DGIN_2, 1)
         self.sdo_write(self.object_dict.DIGITAL_INPUT_CONFIGURATION_DGIN_1, 0)
 
-        # Set the home offset move distance
-        self.sdo_write(self.object_dict.HOME_OFFSET_MOVE_DISTANCE, -622080)
-        getLogger(__name__).debug(f"Configuring device {self} complete.")
+
+
+from typing import NamedTuple
+
+
+# class CSU_motor_config_defaults(NamedTuple):
+#     motor_type: int = 10
+#     nominal_current_ma: int = 1000
+#     output_current_limit_ma: int = 1000
+#     number_of_pole_pairs: int = 7
+#     thermal_time_constant_winding_ms: int = 100
+#     torque_constant_uNm_A: int = 100
+#     max_motor_speed_rpm: int = 1000
+#
+# class CSU_gear_config_defaults(NamedTuple):
+#     gear_reduction_numerator: int = 1
+#     gear_reduction_denominator: int = 1
+#     gear_max_input_speed_rpm: int = 1000
+#     orientation: int = 1
+#
+# class CSU_digital_incremental_encoder_config_defaults(NamedTuple):
+#     number_of_pulses_per_turn: int = 1000
+#     encoder_type: int = 1
+#     direction: int = 1
+#     method: int = 1
+#
+# class CSU_ssi_encoder_config_defaults(NamedTuple):
+#     data_rate: int = 1000000
+#     number_of_bits: int = 24
+#     encoding_type: int = 1
+#     direction: int = 1
+#     check_frame: int = 1
+#     timeout_time_ms: int = 1000
+#     number_of_multi_turn_bits: int = 12
+#     number_of_single_turn_bits: int = 12
